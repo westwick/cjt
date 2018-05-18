49edd9e67dc1e5894aa08492b57cb47154342d1d
Andrew Westwick <awestwick@gmail.com>
2018-05-18 10:55:08 -0500

    initial commit

---
+++ pages/cool.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <h1>cool</h1>
+    <p>{{ title }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    // const postPromise = process.BROWSER_BUILD
+    //   ? import('~/content/blog/posts/' + params.slug + '.json')
+    //   : Promise.resolve(
+    //       require('~/content/blog/posts/' + params.slug + '.json')
+    //     );
+
+    let post = await import('~/content/pages/cool.json');
+    return post;
+  }
+};
+</script>
