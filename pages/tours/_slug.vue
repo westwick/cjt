2f901acd20df2a794506fd1165920c2c0cd033b8
Andrew Westwick <andrewwestwick@iheartmedia.com>
2018-06-13 18:49:48 -0500

    initial tour page

--- pages/tours/_slug.vue
+++ pages/tours/_slug.vue
@@ -1,12 +1,39 @@
 <template>
-  <article>
-    <h1>{{ title }}</h1>
-    <p>Price: {{ price }}</p>
-  </article>
+  <section class="tour-page">
+    <div class="container">
+      <div class="back-to-page"><nuxt-link to="/tours">Browse All Tours</nuxt-link></div>
+      <h1>{{ title }}</h1>
+      <div class="columns">
+        <div class="column is-8">
+          <div class="img-container-container">
+            <div class="img-container">
+              <img class="img" :src="thumbnail">
+            </div>
+          </div>
+        </div>
+        <div class="column is-4">
+          <h2>Price Per Person</h2>
+          <p class="main-tour-info">${{ price }}
+          <h2 class="start-time">Start Time</h2>
+          <p class="main-tour-info">9 - 9:30am</p>
+          <button class="button book-now">Book Now</button>
+          <div class="contact">
+            <p><span class="icon-phone"></span> (805) 564-1819</p>
+            <p><i class="far fa-envelope"></i> hello@captainjackstours.com</p>
+          </div>
+          <vue-markdown>{{ body }}</vue-markdown>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'; 
 export default {
+  components: {
+    VueMarkdown
+  },
   async asyncData({ params }) {
     // const postPromise = process.BROWSER_BUILD
     //   ? import('~/content/blog/posts/' + params.slug + '.json')
@@ -19,3 +46,7 @@ export default {
   }
 };
 </script>
+
+<style scoped lang="sass">
+
+</style>
