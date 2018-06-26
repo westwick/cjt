bb06c2f5acc800338913fb4d8b0c1681d748f712
Andrew Westwick <awestwick@gmail.com>
2018-06-25 20:46:40 -0500

    testing PWA module

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ var dynamicRoutes = getDynamicPaths({
 
 
 module.exports = {
+  modules: [
+    '@nuxtjs/pwa',
+  ],
   /*
   ** Headers of the page
   */
@@ -22,16 +25,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js + Netlify CMS project' },
-      { name: 'theme-color', content: '#ffffff' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' }
+
     ]
   },
   css: [
