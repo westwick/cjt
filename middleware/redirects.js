35b7fd36179f375cd4287583cee3a366c3aaea42
Andrew Westwick <awestwick@gmail.com>
2018-07-22 18:29:42 -0500

    add more redirects

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -4,4 +4,24 @@ export default function ({params, route, redirect}) {
     if (route.path === '/rates.html') {
         redirect('301', '/tours');
     }
+
+    if (route.path === '/privatechartercruises.html') {
+        redirect('301', '/boat-charters');
+    }
+
+    if (route.path === '/events.html') {
+        redirect('301', '/group-events');
+    }
+
+    if (route.path === '/specials.html') {
+        redirect('301', '/specials');
+    }
+
+    if (route.path === '/links.html') {
+        redirect('301', '/links');
+    }
+
+    if (route.path === '/faq.html') {
+        redirect('301', '/faq');
+    }
 }
