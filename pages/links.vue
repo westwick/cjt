fe6e67d82270385d2d9ecb49c1f9644613bc092a
Andrew Westwick <westwick@users.noreply.github.com>
2018-07-23 11:37:03 -0500

    fix links that were breaking because of https

--- pages/links.vue
+++ pages/links.vue
@@ -9,41 +9,41 @@
 
           <h2>Hotel and Lodging Recommendations</h2>
           <ul>
-            <li>La Quinta Inn &amp; Suites - <a href="//www.laquintasantabarbaraca.com" target="_blank">www.laquintasantabarbaraca.com</a></li>
+            <li>La Quinta Inn &amp; Suites - <a href="http://www.laquintasantabarbaraca.com" target="_blank">www.laquintasantabarbaraca.com</a></li>
             <li>Vacation Rentals (VRBO) - Two great rental properties, great alternatives to a hotel: 
                 <a href="http://www.vrbo.com/323449" target="_blank">#323449</a>, 
                 <a href="http://localhost:3000/links" target="_blank">#358945</a>
             <li>
-            <li>Ramada Limited - <a href="//www.sbramada.com" target="_blank">www.sbramada.com</a></li>
-            <li>Old Yacht Club Inn - <a href="//www.oldyachtclubinn.com" target="_blank">www.oldyachtclubinn.com</a></li>
-            <li>Best Western Beachside - <a href="//www.beachsideinn.com" target="_blank">www.beachsideinn.com</a></li>
-            <li>Pacfica Suites - <a href="//www.pacificasuites.com" target="_blank">www.pacificasuites.com</a></li>
-            <li>Eagle Inn - <a href="//www.theeagleinn.com" target="_blank">www.theeagleinn.com</a></li>
-            <li>Holiday Inn Express - <a href="//www.hotelvirginia.com" target="_blank">www.hotelvirginia.com</a></li>
-            <li>Sandpiper Lodge - <a href="//www.sandpiperlodge.com" target="_blank">www.sandpiperlodge.com</a></li>
-            <li>Santa Barbara Vacation Rentals - <a href="//www.sbbeachrentals.com" target="_blank">www.sbbeachrentals.com</a></li>
-            <li>Santa Barbara Travel Resources - <a href="//www.vacationhomerentals.com/vacation-rentals/Santa-Barbara-Central-Coast-California.htm" target="_blank">Santa Barbara Vacation Home Rentals</a></li>
-            <li>Pacific Crest - <a href="//www.pacificcrestsantabarbara.com" target="_blank">www.pacificcrestsantabarbara.com</a></li>
+            <li>Ramada Limited - <a href="http://www.sbramada.com" target="_blank">www.sbramada.com</a></li>
+            <li>Old Yacht Club Inn - <a href="http://www.oldyachtclubinn.com" target="_blank">www.oldyachtclubinn.com</a></li>
+            <li>Best Western Beachside - <a href="http://www.beachsideinn.com" target="_blank">www.beachsideinn.com</a></li>
+            <li>Pacfica Suites - <a href="http://www.pacificasuites.com" target="_blank">www.pacificasuites.com</a></li>
+            <li>Eagle Inn - <a href="http://www.theeagleinn.com" target="_blank">www.theeagleinn.com</a></li>
+            <li>Holiday Inn Express - <a href="http://www.hotelvirginia.com" target="_blank">www.hotelvirginia.com</a></li>
+            <li>Sandpiper Lodge - <a href="http://www.sandpiperlodge.com" target="_blank">www.sandpiperlodge.com</a></li>
+            <li>Santa Barbara Vacation Rentals - <a href="http://www.sbbeachrentals.com" target="_blank">www.sbbeachrentals.com</a></li>
+            <li>Santa Barbara Travel Resources - <a href="http://www.vacationhomerentals.com/vacation-rentals/Santa-Barbara-Central-Coast-California.htm" target="_blank">Santa Barbara Vacation Home Rentals</a></li>
+            <li>Pacific Crest - <a href="http://www.pacificcrestsantabarbara.com" target="_blank">www.pacificcrestsantabarbara.com</a></li>
           </ul>
 
           <h2>Restaurant Recommendations</h2>
           <ul>
-            <li>The Brewhouse - <a href="//www.brewhousesb.com" target="_blank">www.brewhousesb.com</a> - American</li>
-            <li>Brophy Bros - <a href="//www.brophybros.com" target="_blank">www.brophybros.com</a> - Seafood</li>
-            <li>Harrys - <a href="//www.harryssb.com" target="_blank">www.harryssb.com</a> - Steakhouse</li>
-            <li>Pascucci - <a href="//www.pascuccirestaurant.com" target="_blank">www.pascuccirestaurant.com</a> - Italian</li>
-            <li>Ca Dario - <a href="//www.cadario.net" target="_blank">www.cadario.net</a> - Italian</li>
-            <li>Rose Cafe - <a href="//www.rosecafe2.com" target="_blank">www.rosecafe2.com</a> - Mexican</li>
+            <li>The Brewhouse - <a href="http://www.brewhousesb.com" target="_blank">www.brewhousesb.com</a> - American</li>
+            <li>Brophy Bros - <a href="http://www.brophybros.com" target="_blank">www.brophybros.com</a> - Seafood</li>
+            <li>Harrys - <a href="http://www.harryssb.com" target="_blank">www.harryssb.com</a> - Steakhouse</li>
+            <li>Pascucci - <a href="http://www.pascuccirestaurant.com" target="_blank">www.pascuccirestaurant.com</a> - Italian</li>
+            <li>Ca Dario - <a href="http://www.cadario.net" target="_blank">www.cadario.net</a> - Italian</li>
+            <li>Rose Cafe - <a href="http://www.rosecafe2.com" target="_blank">www.rosecafe2.com</a> - Mexican</li>
           </ul>
 
           <h2>Other Santa Barbara Activities &amp; Attractions</h2>
           <ul>
-            <li>Botanic Garden - <a href="//www.sbbg.org" target="_blank">www.sbbg.org</a></li>
-            <li>Lotus Land - <a href="//www.lotusland.org" target="_blank">www.lotusland.org</a></li>
-            <li>Santa Barbara Zoo - <a href="//www.sbzoo.org" target="_blank">www.sbzoo.org</a></li>
-            <li>Museum of Natural History - <a href="//www.sbnature.org" target="_blank">www.sbnature.org</a></li>
-            <li>Ty Warner Sea Center - <a href="//www.sbnature.org/seacenter" target="_blank">www.sbnature.org/seacenter</a></li>
-            <li>Maritime Museum - <a href="//www.sbmm.org" target="_blank">www.sbmm.org</a></li>
+            <li>Botanic Garden - <a href="http://www.sbbg.org" target="_blank">www.sbbg.org</a></li>
+            <li>Lotus Land - <a href="http://www.lotusland.org" target="_blank">www.lotusland.org</a></li>
+            <li>Santa Barbara Zoo - <a href="http://www.sbzoo.org" target="_blank">www.sbzoo.org</a></li>
+            <li>Museum of Natural History - <a href="http://www.sbnature.org" target="_blank">www.sbnature.org</a></li>
+            <li>Ty Warner Sea Center - <a href="http://www.sbnature.org/seacenter" target="_blank">www.sbnature.org/seacenter</a></li>
+            <li>Maritime Museum - <a href="http://www.sbmm.org" target="_blank">www.sbmm.org</a></li>
           </ul>
           </div>
         </div>
