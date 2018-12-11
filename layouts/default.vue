8df3a01359957da0ced626a26006873346d4a3a0
Andrew Westwick <andrewwestwick@iheartmedia.com>
2018-12-11 10:20:14 -0600

    add gift cert modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,13 @@
     <section id="main">
       <nuxt/>
     </section>
+    <div class="holiday-special" v-if="showModal">
+      <a href="#close" class="close" @click="closeModal()">Close</a>
+      <h4>Gift Certificates!</h4>
+      <h5>Give the Gift of Adventure!!!</h5>
+      <p>Gift certificates can be emailed in minutes. Call 805-564-1819 to order.
+        People remember a great experience forever... What did you get as a gift last year??? Call now!</p>
+    </div>
     <app-footer />
   </div>
 </template>
@@ -17,10 +24,46 @@ export default {
     AppHeader,
     AppFooter
   },
+  data() {
+    return {
+      showModal: true
+    }
+  },
+  methods: {
+    closeModal() {
+      this.showModal = false;
+    }
+  }
 }
 </script>
 
 <style lang="sass">
   @import './icon.css'
   @import './hamburger.css'
+
+  .holiday-special
+    min-width: 300px
+    max-width: 500px
+    position: fixed
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    text-align: center
+    background: #fff
+    padding: 3rem
+    border: 1px solid #E22813
+    border-radius: 6px
+    z-index: 999999
+    .close
+      position: absolute
+      top: 10px
+      right: 10px
+    h4
+      color: #E22813
+      font-size: 24px
+      margin-bottom: 1rem
+    h5
+      color: #0D1521
+      font-size: 20px
+      margin-bottom: .5rem
 </style>
