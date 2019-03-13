28b15c516e8e92895a0419a0ccc2395b9996162b
Andrew Westwick <andrewwestwick@iheartmedia.com>
2019-03-12 22:34:24 -0500

    test staleWhileRevalidate

--- nuxt.config.js
+++ nuxt.config.js
@@ -138,15 +138,15 @@ module.exports = {
   workbox: {
     runtimeCaching: [{
       urlPattern: 'https://ucarecdn.com/.*',
-      handler: 'cacheFirst',
+      handler: 'staleWhileRevalidate',
       method: 'GET',
       strategyOptions: {
         cacheName: 'ucare-cdn-images',
-        cacheableResponse: { statuses: [0, 200] },
-        cacheExpiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 3000
-        }
+        // cacheableResponse: { statuses: [0, 200] },
+        // cacheExpiration: {
+        //   maxEntries: 50,
+        //   maxAgeSeconds: 3000
+        // }
       }
     }]
   }
